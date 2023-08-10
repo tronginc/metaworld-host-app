edece8cb5781ff4b8cba1fb2b22a7335e3a837a4
tronginc <tronginc@gmail.com>
2023-08-11 01:22:05 +0700

    add SwiperFlatList component

--- src/components/UI/Screen.tsx
+++ src/components/UI/Screen.tsx
@@ -1,16 +1,41 @@
 import React, { PropsWithChildren, useMemo } from 'react';
-import { StyleSheet, View, ViewStyle } from 'react-native';
+import { StyleSheet, ViewStyle } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-type Props = ViewStyle;
+type Props = ViewStyle & {
+  safeAreaEdge?: keyof typeof safeAreaEdges;
+};
+
+const safeAreaEdges = {
+  top: ['top'],
+  bottom: ['bottom'],
+  left: ['left'],
+  right: ['right'],
+  all: ['top', 'bottom', 'left', 'right'],
+  top_bottom: ['top', 'bottom'],
+  left_right: ['left', 'right'],
+  top_left_right: ['top', 'left', 'right'],
+  bottom_left_right: ['bottom', 'left', 'right'],
+  none: [],
+} as const;
 
 const Screen: React.FC<PropsWithChildren<Props>> = ({
   children,
+  safeAreaEdge = 'none',
   ...styleProps
 }) => {
   const style = useMemo(() => {
     return StyleSheet.flatten([styles.screen, styleProps]);
   }, [styleProps]);
-  return <View style={style}>{children}</View>;
+  const safeAreaEdgesToApply = useMemo(() => {
+    return safeAreaEdges[safeAreaEdge];
+  }, [safeAreaEdge]);
+
+  return (
+    <SafeAreaView edges={safeAreaEdgesToApply} style={style}>
+      {children}
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
--- src/features/Onboarding/screens/OnboardingScreen.tsx
+++ src/features/Onboarding/screens/OnboardingScreen.tsx
@@ -1,74 +1,41 @@
-import React, { useEffect, useMemo } from 'react';
+import React, { useMemo, ComponentProps } from 'react';
 import { useTranslation } from 'react-i18next';
-import { OnboardFlow, OnboardFlowProps } from 'react-native-onboard';
-import { Image, StyleSheet } from 'react-native';
 import Screen from '@components/UI/Screen';
+import OnboardFlow from '@features/components/OnboardFlow';
+import train_to_earn from '@assets/images/onboarding/train_to_earn.png';
+import manage_assets from '@assets/images/onboarding/manage_assets.png';
+import discover_app from '@assets/images/onboarding/discover_app.png';
 
 type Props = {};
 
 const OnboardingScreen: React.FC<Props> = ({}) => {
   const [t] = useTranslation('onboarding');
 
-  const pages = useMemo<OnboardFlowProps['pages']>(() => {
+  const pages = useMemo<ComponentProps<typeof OnboardFlow>['pages']>(() => {
     return [
       {
-        id: '1',
         title: t('contents.page_1.title'),
         subtitle: t('contents.page_1.description'),
-        imageUri: Image.resolveAssetSource(
-          require('@assets/images/onboarding/train_to_earn.png'),
-        ).uri,
+        imageSource: train_to_earn,
       },
       {
-        id: '2',
         title: t('contents.page_2.title'),
         subtitle: t('contents.page_2.description'),
-        imageUri: Image.resolveAssetSource(
-          require('@assets/images/onboarding/manage_assets.png'),
-        ).uri,
+        imageSource: manage_assets,
       },
       {
-        id: '3',
         title: t('contents.page_3.title'),
         subtitle: t('contents.page_3.description'),
-        imageUri: Image.resolveAssetSource(
-          require('@assets/images/onboarding/discover_app.png'),
-        ).uri,
+        imageSource: discover_app,
       },
     ];
   }, [t]);
 
-  useEffect(() => {
-    console.log('OnboardingScreen mounted');
-    return () => {
-      console.log('OnboardingScreen unmounted');
-    };
-  }, []);
-
   return (
-    <Screen backgroundColor="#070C21">
-      <OnboardFlow
-        titleStyle={styles.title}
-        textStyle={styles.text}
-        pages={pages}
-        type="inline"
-      />
+    <Screen safeAreaEdge="all" backgroundColor="#070C21">
+      <OnboardFlow pages={pages} />
     </Screen>
   );
 };
-
-const styles = StyleSheet.create({
-  title: {
-    fontFamily: 'HelveticaNowDisplay',
-    fontWeight: 'bold',
-    color: '#FCFCFD',
-    fontSize: 28,
-  },
-  text: {
-    fontFamily: 'HelveticaNowDisplay',
-    color: '#707A8F',
-    fontSize: 14,
-  },
-});
 
 export default OnboardingScreen;
